982b52d0a6feeb249c61a0f047fea70553517363
Andres Ramirez <andresaaramirez@aol.com>
2020-08-13 14:25:05 -0400

    added new buttons and new links

--- assets/script.js
+++ assets/script.js
@@ -13,17 +13,19 @@ const choiceD = document.querySelector('#D');
 const answers = document.querySelector('#answers');
 const wrongMsg = document.querySelector('#wrong');
 const correctMsg = document.querySelector('#correct');
+
 //end of quiz page
 const endQuiz = document.querySelector('.end-quiz');
 const highScore = document.querySelector('#high-score');
 const initialsInput = document.querySelector('#initials');
 const storageForm = document.querySelector('#storage-form');
 const buttonForm = document.querySelector('#submit');
+
 //high scores screen
+const mainScreen = document.querySelector('.mainscreen');
 const scores = document.querySelector('.scores-display');
 let scorelist = document.querySelector('.score-list');
 let highscoreslist = document.querySelector('#highscores')
-const back = document.querySelector('#back');
 
 //questions and answers 
 const questionSet = [
@@ -65,10 +67,42 @@ const questionSet = [
     optionA: "The <body> section",
     optionB: "The <head> section",
     optionC: "css file",
-    optionD: "Both the <head> section and the <body> section are correct",
+    optionD: "Both the <head> section and the <body>",
     correctAnswer: "optionD"
   },
 ];
+
+highScores.addEventListener("click", function () {
+  highScores.classList.add("hide");
+  document.querySelector('.intro').classList.add("hide");
+  document.querySelector('#start').classList.add('hide');
+  mainScreen.classList.remove("hide");
+  scores.classList.remove("hide");
+  storageForm.classList.add("hide");
+})
+
+//start button
+startBtn.addEventListener("click", function() {
+  startTimer();
+  initiateQuiz();
+  generateQuizQuestion();
+});
+
+//timer will countdown as soon as the button is clicked
+function startTimer() {
+    timerInterval = setInterval(function() {
+      secondsLeft--;
+      timer.textContent = secondsLeft;
+    
+      //if timer is at zero display results
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        //end of the quiz
+        displayScore();
+      }
+  
+    }, 1000);
+}
 
 //variables to be used
 var secondsLeft = 60;
@@ -82,10 +116,12 @@ function initiateQuiz() {
   //hides introductory paragraph on homepage
   document.querySelector('.intro').classList.add("hide");
   //hides start button
-  document.querySelector('#start').classList.add('hide')
+  document.querySelector('#start').classList.add('hide');
   //hides questionpage
   questionPage.classList.remove('hide');
-
+  //remove view high scores link
+  highScores.classList.add("hide");
+  
 }
 
 //load question and answers to quiz screen
@@ -139,16 +175,36 @@ function checkAnswer(e) {
       correctMsg.style.display = "none";
     },1000);
    }
-  nextQuestion();
-}
+   nextQuestion();
+  }
+  
+  
+  //will display final score at end of quiz
+  function displayScore () {
+    clearInterval(timerInterval);
+    timer.textContent = 0;
+    highScore.textContent = secondsLeft;
+    document.querySelector('.question-page').style.display ="none";
+    document.querySelector("#high-score").style.display ="block";
+    highScores.classList.remove("hide");
+    //storageForm.classList.add("hide");
+    //document.querySelector("#time").style.display ="none";
+  }
+  
+  //initialize initials input
+  let initials = "";
 
-//will display final score at end of quiz
-function displayScore () {
-  clearInterval(timerInterval);
-  timer.textContent = 0;
-  highScore.textContent = secondsLeft;
-  document.querySelector('.question-page').style.display ="none";
-  document.querySelector("#high-score").style.display ="block";
+  //submit information
+  storageForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    storageForm.classList.add("hide");
+    scores.classList.remove("hide");
+    highScores.classList.add("hide");
+    mainScreen.classList.remove("hide");
+  });
+
+  function displayHighScores () {
+    document.querySelector('.viewhighscores').style.display = "none";
 }
 
 //initialize scoreslist
@@ -157,52 +213,9 @@ var scoreslist= [];
 //update the the scores count
 function renderScores(){
   scoreslist.innerHTML = "";
-  //console.log(scoreslist.innerHTML)
-  
-  //render new li for each score
-  for (let i = 0; i < scoreslist.length; i++) {
-    console.log(scoreslist);
-    var score = scoreslist[i];
-
-    var li = document.createElement('li');
-    li.textContent = score;
-    li.setAttribute("data", i);
-  }
 }
 
-function storeScores() {
-  localStorage.setItem("scoreslist", JSON.stringify(scoreslist));
-  console.log(localStorage.setItem("scoreslist", JSON.stringify(scoreslist)));
-}
 
-//submit information
-storageForm.addEventListener("submit", function(e) {
-  e.preventDefault();
-  storageForm.classList.add("hide");
-  scores.classList.remove("hide");
-});
 
-//timer will countdown as soon as the button is clicked
-function startTimer() {
-    timerInterval = setInterval(function() {
-      secondsLeft--;
-      timer.textContent = secondsLeft;
-    
-      //if timer is at zero display results
-      if(secondsLeft === 0) {
-        clearInterval(timerInterval);
-        //end of the quiz
-        displayScore();
-      }
-  
-    }, 1000);
-}
-
-//start button
-startBtn.addEventListener("click", function() {
-  startTimer();
-  initiateQuiz();
-  generateQuizQuestion();
-});
 
 
